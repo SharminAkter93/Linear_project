e9a2426493fbda99ad217396cf3ba58a17f4d7a7
SharminAkter93 <hayatsharmin93@gmail.com>
2021-06-29 00:34:09 +0600

    Linear search Program done

--- linear_search.py
+++ linear_search.py
@@ -1,1 +1,12 @@
-def linear_search(list, search_number):
+
+def search (list1,key):
+    for i in range(len(list1)):
+        if key == list1[i]:
+            print("key element is found")
+            break
+    else:
+            print("key element is not found")
+list1 = [34,23,5,6,7,1,23,8] 
+print(list1)
+key = int(input("enter the key element:"))   
+search (list1,key)    
