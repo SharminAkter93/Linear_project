d67ed3cd1899399c8aebb971e68b0173e9273c01
SharminAkter93 <hayatsharmin93@gmail.com>
2021-07-01 21:47:37 +0600

    binary search program done

--- Binary_search.py
+++ Binary_search.py
@@ -1,24 +1,24 @@
 # # Binary_search 
-# def binary_searchiter(arr, n):
-#     low = 0
-#     high = len(arr) - 1
-#     mid = 0
-#     while low <= high:
-#         mid = (high + low) // 2
-#         if arr[mid] < n:
-#             low = mid + 1
-#         elif arr[mid] > n:
-#             high = mid - 1
-#         else: 
-#             return mid
-#     return -1
-# arr = [4, 7, 9, 16, 20, 25]
-# n = 10
-# r = binary_searchiter(arr, n)
-# if r != -1:
-#     print("Element found at index", str(r))
-# else: 
-#     print("Element is not present in array")
+def binary_searchiter(arr, n):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < n:
+            low = mid + 1
+        elif arr[mid] > n:
+            high = mid - 1
+        else: 
+            return mid
+    return -1
+arr = [4, 7, 9, 16, 20, 25]
+n = 10
+r = binary_searchiter(arr, n)
+if r != -1:
+    print("Element found at index", str(r))
+else: 
+    print("Element is not present in array")
 
 
 
